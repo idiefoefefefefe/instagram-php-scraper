b97adcc9408a35cdb27a000dae1d0601912080c2
idiefoefefefefe <macacada414@gmail.com>
2025-05-17 15:18:14 -0300

    Update loginWithSessionId.php

--- examples/loginWithSessionId.php
+++ examples/loginWithSessionId.php
@@ -4,7 +4,7 @@ require __DIR__ . '/../../../../vendor/autoload.php';
 use InstagramScraper\Instagram;
 
 $instagram  = Instagram::withCredentials(new \GuzzleHttp\Client(), '', '', null);
-$instagram->loginWithSessionId('9970068671%3AG9gsBMhabRfBcz%3A1');      // 'sessionid' from browser
+$instagram->loginWithSessionId('70209781271%3A1u4L3PFFxXVbqq%3A7%3AAYc1Suvo0A8lY6vnrN2oLXdwEJvz16JuEOqdI7EEKQ');      // 'sessionid' from browser
                                                                         // it can work for a long time, months
                                                                         // and can throw InstagramAuthException
 $posts  = $instagram->getFeed();
